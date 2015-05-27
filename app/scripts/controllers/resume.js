db3460718b56a1e65a7307792a12562b4c787899
Sam Whiteley <sam.whiteley@hotmail.com>
2015-05-27 17:38:16 -0400

    Added resume page, minimal styles, and link.
    Still need to add static resume html

--- app/scripts/controllers/resume.js
+++ app/scripts/controllers/resume.js
@@ -9,9 +9,4 @@
  */
 angular.module('samWhiteleyApp')
   .controller('ResumeCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
   });
